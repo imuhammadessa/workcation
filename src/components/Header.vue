a437bcbf546134ae6101b2ad9bbe00519365e2e0
Muhammad Essa <iMuhammadessa@gmail.com>
2025-05-16 22:05:42 +0500

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <header class="bg-gray-900 p-4 text-white">
+        <h1>Hello</h1>
+    </header>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
