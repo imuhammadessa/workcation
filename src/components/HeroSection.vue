712c8cbd6c9d5fffb250ebe769c32b8046ee494d
Muhammad Essa <iMuhammadessa@gmail.com>
2025-05-21 15:05:40 +0500

    Header & Hero section updated

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -1,13 +1,13 @@
 <template>
-    <section>
-        <div>
-        <img class='h-8 w-auto' src="../assets/images/workcation.svg" alt='workcation' />
-        <h1>You can work from anywhere.</h1>
-        <h2 class='text-indigo-500 font-bold'>Take advantage of it.</h2>
-        <p>Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not on vacation.</p>
+    <section class="flex items-center">
+        <div class="w-1/2 ml-4">
+        <img class='h-12 w-auto' src="../assets/images/workcation.svg" alt='workcation' />
+        <h1 class="text-2xl text-gray-500">You can work from anywhere.</h1>
+        <h2 class='text-indigo-400 font-bold text-5xl mt-2'>Take advantage of it.</h2>
+        <p class="mt-4 max-w-md text-xl text-gray-700">Workcation helps you find work-friendly rentals in beautiful locations so you can enjoy some nice weather even when you're not on vacation.</p>
     </div>
-    <div>
-        <img src='../assets/images/hero-img2.jpg' alt='Hero image' />
+    <div class="w-1/2 mt-4 mr-4">
+        <img class="w-auto rounded" src='../assets/images/hero-img2.jpg' alt='Hero image' />
     </div>
     </section>
 </template>
