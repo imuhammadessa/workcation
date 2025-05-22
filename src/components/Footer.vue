86faae39d18da6895e6b304d2d318fbce4fc5a37
Muhammad Essa <iMuhammadessa@gmail.com>
2025-05-22 12:29:59 +0500

    Layout page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -61,7 +61,7 @@ export default {
     Instagram,
     Twitter,
   },
-  data() {
+ data() {
     return {
       logo: logoImage,
       menuItems: [
--- src/layout/Layout.vue
+++ src/layout/Layout.vue
@@ -1,13 +1,16 @@
-<template lang="">
-    <div>
-        
-    </div>
+<template>
+  <div>
+    <Header />
+    <router-view />
+    <Footer />
+  </div>
 </template>
+
 <script>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+
 export default {
     
 }
 </script>
-<style lang="">
-    
-</style>
